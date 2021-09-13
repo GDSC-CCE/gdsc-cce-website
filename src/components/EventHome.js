aef20e7e1e4466e15a946654cea22502dfde4f0d
thejaskiranps <thejaskiranps@gmail.com>
2021-09-13 14:41:40 +0530

    remove custom container of eventhome.js

--- src/components/EventHome.js
+++ src/components/EventHome.js
@@ -3,18 +3,6 @@ import styled from "styled-components";
 import Event from "../elements/Event";
 import Page from "../elements/Page";
 
-const MContainer = styled.div`
-    min-height: 100vh;
-    width: 100%;
-    background-color: ${props => props.theme.bg};
-    display:flex;
-    flex-direction: row;
-    flex-wrap:wrap;
-    justify-content: space-evenly;
-    align-items: center;
-    padding-top:80px;
-    scroll-snap-align: start;
-`;
 const Container = styled(Page)`
     justify-content: space-evenly;
 `;
